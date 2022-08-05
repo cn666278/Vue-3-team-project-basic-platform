bd13bce299df83f472eb484518186b956c8200c7
韦 露蔓 <940579927@qq.com>
2022-08-05 15:34:50 +0800

    用户管理

--- src/typings/api.d.ts
+++ src/typings/api.d.ts
@@ -1,39 +1,51 @@
 declare namespace defaultType {
-    interface requestDefaultType {
-        targetAPI: string;
-        token?: string;
-        data?: object | string;
-    }
-    
-    interface responseDefaultType<T = any | null> {
-        State: number;
-        Message: string;
-        HasData: boolean;
-        Data: T;
-    }
+  interface requestDefaultType {
+    targetAPI: string;
+    token?: string;
+    data?: object | string;
+  }
+
+  interface responseDefaultType<T = any | null> {
+    State: number;
+    Message: string;
+    HasData: boolean;
+    Data: T;
+  }
 }
 declare namespace login {
-    import type { Ref } from "vue";
-    interface loginRequestType extends ref {
-        userName: string | Ref<string>;
-        passWord: string | Ref<string>;
-    }
-    interface loginResponseType extends defaultType.responseDefaultType {
-        Data: Auth.Token,
-    }
+  import type { Ref } from "vue";
+  interface loginRequestType extends ref {
+    userName: string | Ref<string>;
+    passWord: string | Ref<string>;
+  }
+  interface loginResponseType extends defaultType.responseDefaultType {
+    Data: Auth.Token;
+  }
 }
 declare namespace config {
-    interface sysConfig {
-        apiSite?: string;
-        socketUrl?: string;
-        mapPointCD?: number;
-        filePath?: string;
-        indexURL?: string;
-        isBusinessLogin?: boolean;
-        title?: string;
-        paramA?: string;
-        paramB?: string;
-        paramC?: string;
-        updateDate?: string;
-    }
+  interface sysConfig {
+    apiSite?: string;
+    socketUrl?: string;
+    mapPointCD?: number;
+    filePath?: string;
+    indexURL?: string;
+    isBusinessLogin?: boolean;
+    title?: string;
+    paramA?: string;
+    paramB?: string;
+    paramC?: string;
+    updateDate?: string;
+  }
+}
+// 用户管理
+declare namespace member {
+  interface memberdata {
+    roleId?: string;
+    account?: string;
+    nick?: number;
+    phone?: string;
+    name?: string;
+    currentPage?: number;
+    pageSize?: number;
+  }
 }
