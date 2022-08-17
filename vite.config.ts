b3dbc794be8186e59c3063a01e36a8c6f26133f0
KUTO <271123056@qq.com>
2022-08-17 10:29:35 +0800

    取消打包类型分组(避免打包后相对路径图片问题)

--- vite.config.ts
+++ vite.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
             output: {
                 chunkFileNames: 'static/js/[name]-[hash].js',
                 entryFileNames: 'static/js/[name]-[hash].js',
-                assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+                assetFileNames: 'static/[name]-[hash].[ext]',
             }
         }
     }
