b55b2787398e68c064831ad803da15d37bffca56
KUTO <271123056@qq.com>
2022-08-31 18:00:45 +0800

    添加请求配置,完善地图管理页面

--- src/views/Map/index.vue
+++ src/views/Map/index.vue
@@ -32,16 +32,24 @@
                     @page-size-change="pageSizeHandle"
                 />
             </template>
+            <template #modal>
+                <Dialog
+                    v-model:showModal="showAddEdit"
+                    :option="dialogOption"
+                    @close="onAddEditClose"
+                    @confirm="onAddEditConfirm"
+                >
+                    <BasicForm
+                        @register="addRegister"
+                        @submit="onAddEditSubmit"
+                    ></BasicForm>
+                </Dialog>
+            </template>
         </TableListTemplate>
-        <Dialog
-            v-model:showModel="showAddEdit"
-            :option="dialogOption"
-            @close="onAddEditClose"
-        ></Dialog>
     </div>
 </template>
 <script setup lang="ts">
-import { ref, reactive, h } from "vue";
+import { ref, h } from "vue";
 import { NTag, NButton, DataTableColumn } from "naive-ui";
 import { AppstoreAddOutlined } from "@vicons/antd";
 import type { formSearch } from "@/components/TableListTemplate";
@@ -51,7 +59,9 @@ import formTable from "@/components/FormTable/index.vue";
 import page from "@/components/pagination/index.vue";
 import TableListTemplate from "@/components/TableListTemplate/index.vue";
 import Dialog from "@/components/dialog/index.vue";
+import { BasicForm, FormSchema, useForm } from "@/components/Form/index";
 import { getMapList } from "@/api/map";
+import { getEnumType } from '@/api/login';
 const searchData = ref<defaultType.requestList>({});
 const pageOption = ref<PaginationType>({
     currentPage: 1,
@@ -60,10 +70,12 @@ const pageOption = ref<PaginationType>({
 const tableData = ref<map.mapList[]>([]);
 const showAddEdit = ref(false);
 const addEditTitle = ref("新增地图");
-const dialogOption = reactive({
+const dialogOption = ref({
     ConfirmBtnText: "提交",
     title: addEditTitle,
 });
+const coordinateTypeList = ref<login.enumType[]>([]);
+/**获取表单数据 */
 const getTableData = async (jsonData?: defaultType.requestList) => {
     if (jsonData) {
         jsonData.currentPage = pageOption.value?.currentPage;
@@ -83,15 +95,23 @@ const getTableData = async (jsonData?: defaultType.requestList) => {
     pageOption.value = pageData;
     return Data.data;
 };
+/**获取坐标系枚举 */
+const getCoordinateType = async () => {
+    coordinateTypeList.value = (await getEnumType('ECoordinateType')).Data;
+};
+/**分页页数监听 */
 const pageHandle = (page: number) => {
     pageOption.value.currentPage = page;
     getTableData();
 };
+/**每页页数监听 */
 const pageSizeHandle = (pageSize: number) => {
     pageOption.value.pageSize = pageSize;
     getTableData();
 };
 getTableData();
+getCoordinateType();
+/**表格格式 */
 const columns: DataTableColumn[] = [
     {
         title: "地图名称",
@@ -134,6 +154,7 @@ const columns: DataTableColumn[] = [
         },
     },
 ];
+/**搜索 */
 const search = ref<formSearch[]>([
     {
         label: "关键字搜索",
@@ -142,9 +163,64 @@ const search = ref<formSearch[]>([
         type: "input",
     },
 ]);
+/**表单列 */
+const addEditForm: FormSchema[] = [
+    {
+        field: "id",
+        label: "",
+        giProps: {
+            span: 0,
+        },
+    },
+    {
+        field: "name",
+        label: "地图名称",
+        component: "NInput",
+        componentProps: {
+            placeholder: "请输入地图名称",
+        },
+        giProps: {span: 3},
+        rules: [
+            { required: true, message: "请输入地图名称", trigger: ["blur"] },
+        ],
+    },
+    {
+        field: "code",
+        label: "地图标识",
+        component: "NInput",
+        componentProps: {
+            placeholder: "请输入地图标识",
+        },
+        giProps: {span: 3},
+        rules: [
+            { required: true, message: "请输入地图标识", trigger: ["blur"] },
+        ],
+    },
+    {
+        field: 'coordinateType',
+        label: '坐标系类型',
+        component: "NSelect",
+        componentProps: {
+            // labelFil
+        }
+    }
+];
+/**增删改查表单 */
+const [addRegister, { setFieldsValue, submit }] = useForm({
+    gridProps: { cols: 3 },
+    labelWidth: 100,
+    layout: "horizontal",
+    showActionButtonGroup: false,
+    schemas: addEditForm,
+});
 const onAddEditModal = () => {
     showAddEdit.value = true;
-    console.log(1145141919108);
+};
+const onAddEditConfirm = () => {
+    submit();
+};
+const onAddEditSubmit = (values: Recordable) => {
+    console.log(values);
 };
 const onAddEditClose = () => {
     showAddEdit.value = false;
