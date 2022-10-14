ff5e8b8fe2d826f3f6f843ad6eefa3f6e67c5c7b
韦 露蔓 <940579927@qq.com>
2022-10-14 11:04:57 +0800

    修改表格列表组件

--- src/components/BasicTable/src/hooks/useDataSource.ts
+++ src/components/BasicTable/src/hooks/useDataSource.ts
@@ -5,142 +5,149 @@ import { isBoolean, isFunction, isArray } from '@/utils/is';
 import { APISETTING } from '../const';
 
 export function useDataSource(
-  propsRef: ComputedRef<BasicTableProps>,
-  { getPaginationInfo, setPagination, setLoading, tableData }: any,
-  emit: (arg0: string, arg1: unknown) => void
+    propsRef: ComputedRef<BasicTableProps>,
+    { getPaginationInfo, setPagination, setLoading, tableData }: any,
+    emit: EmitType
 ) {
-  const dataSourceRef = ref<Recordable[]>([]);
+    const dataSourceRef = ref<Recordable[]>([]);
 
-  watchEffect(() => {
-    tableData.value = unref(dataSourceRef);
-  });
+    watchEffect(() => {
+        tableData.value = unref(dataSourceRef);
+    });
 
-  watch(
-    () => unref(propsRef).dataSource,
-    () => {
-      const { dataSource }: any = unref(propsRef);
-      dataSource && (dataSourceRef.value = dataSource);
-    },
-    {
-      immediate: true,
-    }
-  );
-
-  const getRowKey = computed(() => {
-    const { rowKey }: any = unref(propsRef);
-    return rowKey
-      ? rowKey
-      : () => {
-        return 'key';
-      };
-  });
-
-  const getDataSourceRef = computed(() => {
-    const dataSource = unref(dataSourceRef);
-    if (!dataSource || dataSource.length === 0) {
-      return unref(dataSourceRef);
-    }
-    return unref(dataSourceRef);
-  });
-
-  async function fetch(opt?: { [x: string]: any; } | undefined) {
-    try {
-      setLoading(true);
-      const { request, pagination, beforeRequest, afterRequest }: any = unref(propsRef);
-      if (!request) return;
-      //组装分页信息
-      const pageField = APISETTING.pageField;
-      
-      const sizeField = APISETTING.sizeField;
-      const totalField = APISETTING.totalField;
-      const listField = APISETTING.listField;
-      // console.log(listField)
-      let pageParams = {};
-      const { page = 1, pageSize = 10 } = unref(getPaginationInfo) as PaginationProps;
-
-      if ((isBoolean(pagination) && !pagination) || isBoolean(getPaginationInfo)) {
-        pageParams = {};
-      } else {
-        pageParams[pageField] = (opt && opt[pageField]) || page;
-        pageParams[sizeField] = pageSize;
-      }
-
-      let params = {
-        ...pageParams,
-      };
-      if (beforeRequest && isFunction(beforeRequest)) {
-        // The params parameter can be modified by outsiders
-        params = (await beforeRequest(params)) || params;
-      }
-      const res = await request(params);
-      // console.log(res.Data[totalField])
-      const resultTotal = res.Data[totalField] || 0;
-      const currentPage = res.Data[pageField];
-
-      // 如果数据异常，需获取正确的页码再次执行
-      if (resultTotal) {
-        if (page > resultTotal) {
-          setPagination({
-            [pageField]: resultTotal,
-          });
-          fetch(opt);
+    watch(
+        () => unref(propsRef).dataSource,
+        () => {
+            const { dataSource }: any = unref(propsRef);
+            dataSource && (dataSourceRef.value = dataSource);
+        },
+        {
+            immediate: true,
         }
-      }
-      let resultInfo = res.Data[listField] ? res.Data[listField] : [];
-      if (afterRequest && isFunction(afterRequest)) {
-        // can modify the data returned by the interface for processing
-        resultInfo = (await afterRequest(resultInfo)) || resultInfo;
-      }
-      dataSourceRef.value = resultInfo;
-      setPagination({
-        [pageField]: currentPage,
-        [totalField]: resultTotal,
-      });
-      if (opt && opt[pageField]) {
-        setPagination({
-          [pageField]: opt[pageField] || 1,
-        });
-      }
-      emit('fetch-success', {
-        items: unref(resultInfo),
-        resultTotal,
-      });
-    } catch (error) {
-      console.error(error);
-      emit('fetch-error', error);
-      dataSourceRef.value = [];
-      // setPagination({
-      //   pageCount: 0,
-      // });
-    } finally {
-      setLoading(false);
+    );
+
+    const getRowKey = computed(() => {
+        const { rowKey }: any = unref(propsRef);
+        return rowKey
+            ? rowKey
+            : () => {
+                  return 'key';
+              };
+    });
+
+    const getDataSourceRef = computed(() => {
+        try {
+            setLoading(true);
+            const dataSource = unref(dataSourceRef);
+            if (!dataSource || dataSource.length === 0) {
+                return unref(dataSourceRef);
+            } else {
+                return unref(dataSourceRef);
+            }
+        } finally {
+            setLoading(false);
+        }
+    });
+
+    async function fetch(opt?: { [x: string]: any } | undefined) {
+        try {
+            setLoading(true);
+            const { request, pagination, beforeRequest, afterRequest }: any = unref(propsRef);
+            if (!request) return;
+            //组装分页信息
+            const pageField = APISETTING.pageField;
+
+            const sizeField = APISETTING.sizeField;
+            const totalField = APISETTING.totalField;
+            const listField = APISETTING.listField;
+            // console.log(listField)
+            let pageParams = {};
+            const { page = 1, pageSize = 10 } = unref(getPaginationInfo) as PaginationProps;
+
+            if ((isBoolean(pagination) && !pagination) || isBoolean(getPaginationInfo)) {
+                pageParams = {};
+            } else {
+                pageParams[pageField] = (opt && opt[pageField]) || page;
+                pageParams[sizeField] = pageSize;
+            }
+
+            let params = {
+                ...pageParams,
+            };
+            if (beforeRequest && isFunction(beforeRequest)) {
+                // The params parameter can be modified by outsiders
+                params = (await beforeRequest(params)) || params;
+            }
+            const res = await request(params);
+            // console.log(res.Data[totalField])
+            const resultTotal = res.Data[totalField] || 0;
+            const currentPage = res.Data[pageField];
+
+            // 如果数据异常，需获取正确的页码再次执行
+            if (resultTotal) {
+                if (page > resultTotal) {
+                    setPagination({
+                        [pageField]: resultTotal,
+                    });
+                    fetch(opt);
+                }
+            }
+            let resultInfo = res.Data[listField] ? res.Data[listField] : [];
+            if (afterRequest && isFunction(afterRequest)) {
+                // can modify the data returned by the interface for processing
+                resultInfo = (await afterRequest(resultInfo)) || resultInfo;
+            }
+            dataSourceRef.value = resultInfo;
+            setPagination({
+                [pageField]: currentPage,
+                [totalField]: resultTotal,
+            });
+            if (opt && opt[pageField]) {
+                setPagination({
+                    [pageField]: opt[pageField] || 1,
+                });
+            }
+            emit('fetch-success', {
+                items: unref(resultInfo),
+                resultTotal,
+            });
+        } catch (error) {
+            console.error(error);
+            emit('fetch-error', error);
+            dataSourceRef.value = [];
+            // setPagination({
+            //   pageCount: 0,
+            // });
+        } finally {
+            setLoading(false);
+        }
     }
-  }
 
-  onMounted(() => {
-    setTimeout(() => {
-      fetch();
-    }, 16);
-  });
+    onMounted(() => {
+        setTimeout(() => {
+            fetch();
+            // getDataSource()
+        }, 16);
+    });
 
-  function setTableData(values: Recordable<any>[]) {
-    dataSourceRef.value = values;
-  }
+    function setTableData(values: Recordable<any>[]) {
+        dataSourceRef.value = values;
+    }
 
-  function getDataSource(): any[] {
-    return getDataSourceRef.value;
-  }
+    function getDataSource(): any[] {
+        return getDataSourceRef.value;
+    }
 
-  async function reload(opt?: { [x: string]: any; } | undefined) {
-    await fetch(opt);
-  }
+    async function reload(opt?: { [x: string]: any } | undefined) {
+        await fetch(opt);
+    }
 
-  return {
-    fetch,
-    getRowKey,
-    getDataSourceRef,
-    getDataSource,
-    setTableData,
-    reload,
-  };
+    return {
+        fetch,
+        getRowKey,
+        getDataSourceRef,
+        getDataSource,
+        setTableData,
+        reload,
+    };
 }
