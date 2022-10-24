8460d8369d5f7fedd08f03c1f47540a582b31b97
KUTO <271123056@qq.com>
2022-10-24 09:56:06 +0800

    优化请求方法

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -1,17 +1,51 @@
 import qs from "qs";
 import Request from "./request";
 import { getToken, removeToken } from "@/utils/auth/index";
-const request = new Request({
-    baseURL: import.meta.env.VITE_BASE_URL,
-    timeout: 1000 * 30,
-});
+interface requestObj {
+    base: Request;
+    business?: Request;
+}
+const request: requestObj = {
+    base: new Request({
+        baseURL: import.meta.env.VITE_BASE_URL,
+        timeout: 1000 * 30,
+    }),
+    // business: new Request({
+    //     baseURL: import.meta.env.VITE_BUSINESS_URL,
+    //     timeout: 1000 * 30,
+    // }),
+};
+// const request = new Request({
+//     baseURL: import.meta.env.VITE_BASE_URL,
+//     timeout: 1000 * 30,
+// });
+const baseTargetAPIList = [
+    "/Admin",
+    "/App",
+    "/AppManage",
+    "/Config",
+    "/DeviceCommand",
+    "/DeviceControl",
+    "/DeviceVideo",
+    "/File",
+    "/Log",
+    "/Login",
+    "/RTVS",
+    "/UnAuth",
+    "/WeiXin",
+];
 
 interface configOption {
     closeMessage?: boolean;
 }
 
-export default <T>(url: string, jsonData: defaultType.requestDefaultType, option?: configOption) => {
+export default <T>(
+    url: string,
+    jsonData: defaultType.requestDefaultType,
+    option?: configOption
+) => {
     let formData = new FormData();
+    let requestAPI = "base";
     if (jsonData.files) {
         formData.append("action", jsonData.targetAPI);
         formData.append("token", getToken());
@@ -20,14 +54,23 @@ export default <T>(url: string, jsonData: defaultType.requestDefaultType, option
             formData.append(file.name, file);
         });
     }
-    return request.request<defaultType.responseDefaultType<T>>({
+    if (
+        baseTargetAPIList.some((Api) => {
+            return url == Api;
+        })
+    ) {
+        requestAPI = "base";
+    } else {
+        requestAPI = "business";
+    }
+    return request[requestAPI].request<defaultType.responseDefaultType<T>>({
         url: url,
         method: "post",
         interceptors: {
             responseInterceptors(config: any) {
                 if (config.State == 1) {
                     if (config.Message != "") {
-                        if(!option?.closeMessage) {
+                        if (!option?.closeMessage) {
                             window.$message?.success(config.Message, {
                                 duration: 3000,
                             });
@@ -44,7 +87,7 @@ export default <T>(url: string, jsonData: defaultType.requestDefaultType, option
                 }
                 return config;
             },
-            responseInterceptorsCatch(error) {
+            responseInterceptorsCatch(error: any) {
                 console.log(error);
             },
         },
